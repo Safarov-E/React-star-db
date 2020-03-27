95ed54c7eba37cea23e8416ec0c14e597370cc21
Safarov <elkhan.safarov10@yandex.com>
2020-03-27 21:54:12 +0500

    Обработка ошибок сети в компоненте

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
@@ -11,11 +12,18 @@ export default class RandomPlanet extends Component {
   }
   state = {
     planet: {},
-    loading: true
+    loading: true,
+    error: false
   }
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
+      loading: false
+    })
+  }
+  onError = (err) => {
+    this.setState({
+      error: true,
       loading: false
     })
   }
@@ -24,17 +32,22 @@ export default class RandomPlanet extends Component {
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
+      .catch(this.onError)
   }
   render() {
-    const {planet, loading} = this.state;
+    const {planet, loading, error} = this.state;
+
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet}/> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
-
     );
   }
 }
