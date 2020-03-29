ea055ca9ce68808dd45bff49c8a8f92f0d01a64d
Safarov <elkhan.safarov10@yandex.com>
2020-03-29 20:58:16 +0500

    рефакторинг компонента

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -24,8 +24,9 @@ export default class ItemList extends Component {
 
   renderItems(arr) {
     return arr.map((item) => {
-      const {id} = item;
-      const label = this.props.renderItem(item)
+      const { id } = item;
+      const label = this.props.children(item);
+
       return (
         <li className="list-group-item"
             key={id}
