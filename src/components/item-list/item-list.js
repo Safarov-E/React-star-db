ed7b97dad2fe8460672f8dc3db079bc6605aa1a1
Safarov <elkhan.safarov10@yandex.com>
2020-03-30 14:56:16 +0500

    Рефакторинг компонентов

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,56 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import './item-list.css';
-import {withData} from '../hoc-helper';
+import { withData } from '../hoc-helpers';
 import SwapiService from "../../services/swapi-service";
+import './item-list.css';
 
 const ItemList = (props) => {
-  const { data, onItemSelected, children: renderLabel } = props;
-    const items = data.map((item) => {
-      const { id } = item;
-      const label = renderLabel(item);
 
-      return (
-        <li className="list-group-item"
-            key={id}
-            onClick={() => onItemSelected(id)}>
-          {label}
-        </li>
-      );
-    });
-  
+  const { data, onItemSelected, children: renderLabel } = props;
+
+  const items = data.map((item) => {
+    const { id } = item;
+    const label = renderLabel(item);
+
+    return (
+      <li className="list-group-item"
+          key={id}
+          onClick={() => onItemSelected(id)}>
+        {label}
+      </li>
+    );
+  });
 
   return (
     <ul className="item-list list-group">
       {items}
     </ul>
   );
-}
-const withData = (View, getData) => {
-  return class extends Component {
-    state = {
-      data: null
-    };
-  
-    componentDidMount() {
-      const { getData } = this.props;
-  
-      getData()
-        .then((data) => {
-          this.setState({
-            data
-          });
-        });
-    }
-    render() {
-      const { data } = this.state;
+};
 
-      if (!data) {
-        return <Spinner />;
-      }
-      return <View {...this.props} data={data}/>
-    }
-  };
-}
 const { getAllPeople } = new SwapiService();
+
 export default withData(ItemList, getAllPeople);
---
+++ src/components/sw-components/details.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import ItemDetails, {Record} from '../item-details';
+import SwapiService from '../../services/swapi-service';
+
+const swapiService = new SwapiService();
+
+const {
+    getPerson,
+    getPlanet,
+    getStarship,
+    getPersonImage,
+    getPlanetImage,
+    getStarshipImage
+} = swapiService;
+
+const PersonDetails = ({itemId}) => {
+    return (
+        <ItemDetails
+            itemId={itemId}
+            getData={getPerson}
+            getImageUrl={getPersonImage} >
+
+            <Record field="population" label="Population" />
+            <Record field="rotationPeriod" label="Rotation Period" />
+            <Record field="diameter" label="Diameter" />
+        </ItemDetails>
+    )
+};
+const PlanetDetails = ({itemId}) => {
+    return (
+        <ItemDetails
+            itemId={itemId}
+            getData={getPlanet}
+            getImageUrl={getPlanetImage}>
+
+            <Record field="model" label="Model" />
+            <Record field="length" label="Length" />
+            <Record field="costInCredits" label="Cost" />
+        </ItemDetails>
+    )
+};
+const StarshipDetails = ({itemId}) => {
+    return (
+        <ItemDetails
+            itemId={itemId}
+            getData={getStarship}
+            getImageUrl={getStarshipImage}>
+
+            <Record field="model" label="Model" />
+            <Record field="length" label="Length" />
+            <Record field="costInCredits" label="Cost" />
+        </ItemDetails>
+    )
+};
+
+export {
+    PersonDetails,
+    PlanetDetails,
+    StarshipDetails
+}
