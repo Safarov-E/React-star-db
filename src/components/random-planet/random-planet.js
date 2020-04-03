29b775b9f54b9b46fb6b0114cca58ac152b4977a
Safarov <elkhan.safarov10@yandex.com>
2020-04-03 22:50:15 +0500

    Библиотеки prop-types

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
@@ -9,13 +10,7 @@ export default class RandomPlanet extends Component {
     updateInterval: 10000
   }
   static propTypes = {
-    updateInterval: (props, propName, componentName) => {
-      const value = props[propName]
-      if(typeof value === 'number' && !isNaN(value)) {
-        return null
-      }
-      return new TypeError(`${componentName}: ${propName} must be number`)
-    }
+    updateInterval: PropTypes.number.isRequired
   }
   swapiService = new SwapiService();
   state = {
