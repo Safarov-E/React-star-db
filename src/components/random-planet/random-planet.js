45b7eb454f633b79e87b4ab2b460a3069c1ce89c
Safarov <elkhan.safarov10@yandex.com>
2020-04-03 19:33:46 +0500

    Свойство defaultProps

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -5,6 +5,9 @@ import ErrorIndicator from '../error-indicator';
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
+  static defaultProps = {
+    updateInterval: 10000
+  }
   swapiService = new SwapiService();
   state = {
     planet: {},
@@ -12,8 +15,9 @@ export default class RandomPlanet extends Component {
     error: false
   }
   componentDidMount(){
+    const { updateInterval } = this.props;
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 2500)
+    this.interval = setInterval(this.updatePlanet, updateInterval)
   }
   componentWillUnmount() {
     clearInterval(this.interval);
