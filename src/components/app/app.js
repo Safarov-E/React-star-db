ee44efac4a3b766c697b27fe7b5ffc8d0713ddb6
Safarov <elkhan.safarov10@yandex.com>
2020-04-09 22:04:42 +0500

    withRouter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,6 +12,7 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
 
@@ -40,9 +41,15 @@ export default class App extends Component {
 
               <RandomPlanet />
               
+              <Route path="/" exact render={() => <h2>Welcome to StarDB</h2>}/>
               <Route path="/people" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" component={StarshipsPage} />
+              <Route path="/starships" exact component={StarshipsPage} />
+              <Route path="/starships/:id"
+                      render={({match}) => {
+                        const {id} = match.params;
+                        return <StarshipDetails itemId={id} />
+                      }}/>
 
             </div>
           </Router>
